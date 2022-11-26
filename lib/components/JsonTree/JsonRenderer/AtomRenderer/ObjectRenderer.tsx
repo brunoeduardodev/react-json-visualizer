0d03282d38adee1bc510f06ebf3cf5d14a6c3707
brunoeduardodev <brunoeduardo.dev@gmail.com>
2022-11-26 00:29:47 +0000

    feat: remove trailing comma on last object item

--- lib/components/JsonTree/JsonRenderer/AtomRenderer/ObjectRenderer.tsx
+++ lib/components/JsonTree/JsonRenderer/AtomRenderer/ObjectRenderer.tsx
@@ -1,4 +1,5 @@
 import React, { useMemo } from "react";
+import { Primitive, ValidJSON } from "../../../../types";
 import { getEntriesWithLines } from "../../../../utils";
 import { Enclosure } from "../Enclosure";
 import { Line } from "../Line";
@@ -11,16 +12,45 @@ type Props = {
   lineOffset: number;
 };
 
+type EntryProps = {
+  entryKey: string;
+  value: Primitive;
+
+  depth: number;
+  line: number;
+  isLast: boolean;
+};
+
+const EntryRenderer = ({
+  entryKey,
+  value,
+  depth,
+  isLast,
+  line,
+}: EntryProps) => {
+  return (
+    <Line depth={depth} line={line}>
+      <KeyRenderer data={entryKey} />: <ValueRenderer data={value} />
+      {isLast ? "" : ","}
+    </Line>
+  );
+};
+
 export const ObjectRenderer = ({ data, lineOffset, depth }: Props) => {
   const isRoot = lineOffset === 0;
   const entries = getEntriesWithLines(data, isRoot ? 2 : lineOffset);
 
   return (
     <Enclosure isRoot={isRoot} toLine={entries.at(-1)?.toLine} type="curly">
-      {entries.map(({ fromLine, key, value }) => (
-        <Line key={`${depth} - ${key}`} depth={depth + 1} line={fromLine}>
-          <KeyRenderer data={key} />: <ValueRenderer data={value} />
-        </Line>
+      {entries.map(({ fromLine, key, value }, index) => (
+        <EntryRenderer
+          key={`${depth}-${key}`}
+          entryKey={key}
+          depth={depth + 1}
+          isLast={index === entries.length - 1}
+          line={fromLine}
+          value={value}
+        />
       ))}
     </Enclosure>
   );
