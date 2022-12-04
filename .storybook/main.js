4a6c45baea56b257624a3ab2623c543e20a115c5
brunoeduardodev <brunoeduardo.dev@gmail.com>
2022-12-04 23:01:20 +0000

    fix: add support to ts paths on sb

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,11 @@
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+
 module.exports = {
   stories: ["../lib/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
   framework: "@storybook/react",
+  webpackFinal: async (config, { configType }) => {
+    config.resolve.plugins = [new TsconfigPathsPlugin()];
+    return config;
+  },
 };
